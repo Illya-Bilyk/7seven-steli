34b57568e04b5588809621b5e13cadae61302c3a
Illya-Bilyk <94546203+Illya-Bilyk@users.noreply.github.com>
2023-08-10 15:11:58 +0000

    Deploying to gh-pages from @ Illya-Bilyk/7seven-steli@5d1e99283030b8b780b190fd3c79817ec25abf64 🚀

---
+++ index.e3284584.js
@@ -0,0 +1,2 @@
+new Swiper(".sample-slider",{loop:!0,autoplay:{delay:2e3},pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.e3284584.js.map
